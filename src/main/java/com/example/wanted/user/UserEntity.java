d73607015502671607901dabcda691ae43c45e90
최가연 <cn01803@naver.com>
2023-10-12 11:26:25 +0900

    Feat: user entity 코드 추가

---
+++ src/main/java/com/example/wanted/user/UserEntity.java
@@ -0,0 +1,33 @@
+package com.example.wanted.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "user")
+public class UserEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
+
+	@Column(name = "user_name")
+	private String name;
+
+}
