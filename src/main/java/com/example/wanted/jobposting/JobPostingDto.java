f97f94770d23c356bfe587b63077b5891b14097b
최가연 <cn01803@naver.com>
2023-10-12 20:35:45 +0900

    Feat: JobPostingDto 생성

---
+++ src/main/java/com/example/wanted/jobposting/JobPostingDto.java
@@ -0,0 +1,14 @@
+package com.example.wanted.jobposting;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class JobPostingDto {
+	private Long companyId;
+	private String position;
+	private int reward;
+	private String content;
+	private String techStack;
+}
