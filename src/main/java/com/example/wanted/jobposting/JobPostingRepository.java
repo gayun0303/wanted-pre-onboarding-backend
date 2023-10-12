be743961bf07cf4440c55502b7645202a504c8f3
최가연 <cn01803@naver.com>
2023-10-12 20:37:04 +0900

    Feat: JobPostingRepository 생성

---
+++ src/main/java/com/example/wanted/jobposting/JobPostingRepository.java
@@ -0,0 +1,7 @@
+package com.example.wanted.jobposting;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobPostingRepository extends JpaRepository<JobPostingEntity, Long> {
+
+}
