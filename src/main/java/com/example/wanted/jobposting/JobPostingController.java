060a2406751e85c25199cd52254b2f19ede5c141
최가연 <cn01803@naver.com>
2023-10-12 21:28:25 +0900

    Feat: JobPostingController: 채용 공고 수정 컨트롤러

--- src/main/java/com/example/wanted/jobposting/JobPostingController.java
+++ src/main/java/com/example/wanted/jobposting/JobPostingController.java
@@ -5,7 +5,9 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +31,20 @@ public class JobPostingController {
 			return new ResponseEntity<>(result, HttpStatus.OK);
 		} catch (Exception e) {
 			log.error("채용 공고 등록 실패 {}", e.getMessage());
+			result.put("state", "FAIL");
+			return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
+		}
+	}
+
+	@PutMapping("/{jobPostingId}")
+	public ResponseEntity<?> updateJobPosting(@PathVariable Long jobPostingId, @RequestBody JobPostingUpdateDto jobPostingUpdateDto) {
+		Map<String, Object> result = new HashMap<>();
+		try {
+			jobPostingService.updateJobPosting(jobPostingUpdateDto, jobPostingId);
+			result.put("state", "SUCCESS");
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			log.error("채용 공고 수정 실패 {}", e.getMessage());
 			result.put("state", "FAIL");
 			return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
 		}
