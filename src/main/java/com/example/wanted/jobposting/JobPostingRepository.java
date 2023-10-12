5920feb9b78bd6a94aefe87f4c1a15ea6efcfc4d
최가연 <cn01803@naver.com>
2023-10-12 21:33:42 +0900

    Feat: JobPostingRepository: 채용 공고 삭제 메소드

--- src/main/java/com/example/wanted/jobposting/JobPostingRepository.java
+++ src/main/java/com/example/wanted/jobposting/JobPostingRepository.java
@@ -3,5 +3,5 @@ package com.example.wanted.jobposting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobPostingRepository extends JpaRepository<JobPostingEntity, Long> {
-
+	void deleteByJobPostingId(Long jobPostingId);
 }
