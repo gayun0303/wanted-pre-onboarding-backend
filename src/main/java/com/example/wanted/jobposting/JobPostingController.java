babd1af69b71eb6ee4f604e74eef2de53fe93d1d
최가연 <cn01803@naver.com>
2023-10-14 10:54:34 +0900

    Feat: JobPostingContoller: 채용 공고 삭제 컨트롤러

--- src/main/java/com/example/wanted/jobposting/JobPostingController.java
+++ src/main/java/com/example/wanted/jobposting/JobPostingController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,20 @@ public class JobPostingController {
 			return new ResponseEntity<>(result, HttpStatus.OK);
 		} catch (Exception e) {
 			log.error("채용 공고 수정 실패 {}", e.getMessage());
+			result.put("state", "FAIL");
+			return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
+		}
+	}
+
+	@DeleteMapping("/{jobPostingId}")
+	public ResponseEntity<?> deleteJobPosting(@PathVariable Long jobPostingId) {
+		Map<String, Object> result = new HashMap<>();
+		try {
+			jobPostingService.deleteJobPosting(jobPostingId);
+			result.put("state", "SUCCESS");
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			log.error("채용 공고 삭제 실패 {}", e.getMessage());
 			result.put("state", "FAIL");
 			return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
 		}
