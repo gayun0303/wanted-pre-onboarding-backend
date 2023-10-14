10e625accc59feefb933b3b5bb32652c1dbdb242
최가연 <cn01803@naver.com>
2023-10-14 11:49:58 +0900

    Feat: JobPostingController: 채용 공고 키워드 검색 컨트롤러

--- src/main/java/com/example/wanted/jobposting/JobPostingController.java
+++ src/main/java/com/example/wanted/jobposting/JobPostingController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -75,6 +76,20 @@ public class JobPostingController {
 			return new ResponseEntity<>(result, HttpStatus.OK);
 		} catch (Exception e) {
 			log.error("채용 공고 가져오기 실패 {}", e.getMessage());
+			result.put("state", "FAIL");
+			return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
+		}
+	}
+
+	@GetMapping("/search")
+	public ResponseEntity<?> searchJobPostingList(@RequestParam("keyword") String searchKeyword) {
+		Map<String, Object> result = new HashMap<>();
+		try {
+			result.put("result", jobPostingService.searchJobPostingList(searchKeyword));
+			result.put("state", "SUCCESS");
+			return new ResponseEntity<>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			log.error("채용 공고 검색 실패 {}", e.getMessage());
 			result.put("state", "FAIL");
 			return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
 		}
