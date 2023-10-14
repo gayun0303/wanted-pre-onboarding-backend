149084853348a09a8b8084fe0a61a8290bb80fe1
최가연 <cn01803@naver.com>
2023-10-14 10:28:33 +0900

    Feat: JobPostingRepository: 채용 공고 리스트 조회

--- src/main/java/com/example/wanted/jobposting/JobPostingRepository.java
+++ src/main/java/com/example/wanted/jobposting/JobPostingRepository.java
@@ -1,7 +1,14 @@
 package com.example.wanted.jobposting;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JobPostingRepository extends JpaRepository<JobPostingEntity, Long> {
 	void deleteByJobPostingId(Long jobPostingId);
+
+	@Query(value = "SELECT jp.job_posting_id AS jobPostingId, c.company_name AS companyName, c.country, c.region, jp.position, jp.reward, jp.tech_stack AS techStack "
+		+ "FROM job_posting AS jp, company AS c;", nativeQuery = true)
+	List<JobPostingDto> findAllJobPosting();
 }
